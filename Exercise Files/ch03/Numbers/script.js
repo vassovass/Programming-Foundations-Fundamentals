f78185fc32f4c143f68931aea614324a1083b250
vassovass <vasso@vaseo.co.za>
2018-12-24 01:20:01 +0200

    Learning about properties with JS, Length property

---
+++ Exercise Files/ch03/Numbers/script.js
@@ -0,0 +1,8 @@
+var a = 5;
+var a = 20;
+var a = -25000;
+var a = -350.04040;
+alert(a);
+
+var phrase = "this is a phrase to test the length property in JS. ";
+alert(phrase + (phrase.length - 1) + " Characters");
